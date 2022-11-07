fc09db18cc350a83893e146d4363015c5edf3a68
gemintangsf <gemintang@inovasi360.com>
2022-11-07 23:58:56 +0700

    :hammer: Backend: Membuat API Registrasi

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  def not_found
+      render json: { error: 'routes tidak dapat ditemukan!' }
+    end
+  
+    def authorize_request
+      header = request.headers['Authorization']
+      header = header.split(' ').last if header
+      begin
+        @decoded = JsonWebToken.decode(header)
+        @current_user = User.find(@decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: e.message + '. User yang anda cari tidak dapat ditemukan' }, status: :unauthorized
+      rescue JWT::DecodeError => e
+        render json: { errors: e.message + ', Login terlebih dahulu!' }, status: :unauthorized
+      end
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  resources :users, param: :_email
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
+
+  # resources :users, param: :user_params
+  # post '/users', to: 'users#create'
 end
