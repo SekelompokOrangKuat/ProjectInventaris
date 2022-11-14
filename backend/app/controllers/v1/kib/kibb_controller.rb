ee607c9e1bb6fa822fd75cd007efc80837e6f003
gemintangsf <gemintang@inovasi360.com>
2022-11-14 15:53:35 +0700

    :hammer: Backend: Autorize KIB

--- backend/app/controllers/v1/kib/kibb_controller.rb
+++ backend/app/controllers/v1/kib/kibb_controller.rb
@@ -26,102 +26,114 @@ class V1::Kib::KibbController < ApplicationController
     end
   
     def create
-        @kib_b = Barang::Kibb.new(user_params)
-        if @kib_b.save
-            render json: @kib_b, status: :created
+        if role.match(/Pengguna/).present?
+            render json: {role: role, error:"Tidak memiliki akses!"}, status: :unauthorized
         else
-            render json: { errors: @kib_b.errors.full_messages }, status: :unprocessable_entity
+            @kib_b = Barang::Kibb.new(user_params)
+            if @kib_b.save
+                render json: @kib_b, status: :created
+            else
+                render json: { errors: @kib_b.errors.full_messages }, status: :unprocessable_entity
+            end
         end
     end
 
     def edit
-        if params[:id].blank?
-            render json: {error: "Id tidak boleh kosong!"}, status: :unprocessable_entity
-        else 
-            kib_validate = Barang::Kiba.where(_id: params[:id]).first
-            if not kib_validate.present?
-                render json: {error: "Id tidak dapat ditemukan!"}, status: :unprocessable_entity
-            elsif kib_validate.status_kib == Enums::KibStatus::DELETED
-                render json: {error: "Barang sudah dihapus!"}, status: :unprocessable_entity
-            else
-                begin
-                    @kib_b = Barang::Kiba.find(params[:id])
-                    kode_lokasi = params[:kode_lokasi]
-                    nama_barang = params[:nama_barang]
-                    nomor_register = params[:nomor_register]
-                    tipe_barang = params[:tipe_barang]
-                    bahan_barang = params[:bahan_barang]
-                    tahun_pembelian = params[:tahun_pembelian]
-                    nomor_pabrik = params[:nomor_pabrik]
-                    nomor_rangka = params[:nomor_rangka]
-                    nomor_mesin = params[:nomor_mesin]
-                    nomor_polisi = params[:nomor_polisi]
-                    asal_usul = params[:asal_usul]
-                    harga_barang = params[:harga_barang]
-                    keterangan = params[:keterangan]
-                    if params[:kode_lokasi].blank? 
-                        kode_lokasi = @kib_b.kode_lokasi
+        if role.match(/Pengguna/).present?
+            render json: {role: role, error:"Tidak memiliki akses!"}, status: :unauthorized
+        else
+            if params[:id].blank?
+                render json: {error: "Id tidak boleh kosong!"}, status: :unprocessable_entity
+            else 
+                kib_validate = Barang::Kiba.where(_id: params[:id]).first
+                if not kib_validate.present?
+                    render json: {error: "Id tidak dapat ditemukan!"}, status: :unprocessable_entity
+                elsif kib_validate.status_kib == Enums::KibStatus::DELETED
+                    render json: {error: "Barang sudah dihapus!"}, status: :unprocessable_entity
+                else
+                    begin
+                        @kib_b = Barang::Kiba.find(params[:id])
+                        kode_lokasi = params[:kode_lokasi]
+                        nama_barang = params[:nama_barang]
+                        nomor_register = params[:nomor_register]
+                        tipe_barang = params[:tipe_barang]
+                        bahan_barang = params[:bahan_barang]
+                        tahun_pembelian = params[:tahun_pembelian]
+                        nomor_pabrik = params[:nomor_pabrik]
+                        nomor_rangka = params[:nomor_rangka]
+                        nomor_mesin = params[:nomor_mesin]
+                        nomor_polisi = params[:nomor_polisi]
+                        asal_usul = params[:asal_usul]
+                        harga_barang = params[:harga_barang]
+                        keterangan = params[:keterangan]
+                        if params[:kode_lokasi].blank? 
+                            kode_lokasi = @kib_b.kode_lokasi
+                        end
+                        if params[:nama_barang].blank?
+                            nama_barang = @kib_b.nama_barang
+                        end
+                        if params[:nomor_register].blank?
+                            nomor_register = @kib_b.nomor_register
+                        end
+                        if params[:tipe_barang].blank?
+                            tipe_barang = @kib_b.tipe_barang
+                        end
+                        if params[:bahan_barang].blank?
+                            bahan_barang = @kib_b.bahan_barang
+                        end
+                        if params[:tahun_pembelian].blank? 
+                            tahun_pembelian = @kib_b.tahun_pembelian
+                        end
+                        if params[:nomor_pabrik].blank?
+                            nomor_pabrik = @kib_b.nomor_pabrik
+                        end
+                        if params[:nomor_rangka].blank?
+                            nomor_rangka = @kib_b.nomor_rangka
+                        end
+                        if params[:nomor_mesin].blank?
+                            nomor_mesin = @kib_b.nomor_mesin
+                        end
+                        if params[:nomor_polisi].blank?
+                            nomor_polisi = @kib_b.nomor_polisi
+                        end
+                        if params[:asal_usul].blank?
+                            asal_usul = @kib_b.asal_usul
+                        end
+                        if params[:harga_barang].blank?
+                            harga_barang = @kib_b.harga_barang
+                        end
+                        if params[:keterangan].blank?
+                            keterangan = @kib_b.keterangan
+                        end
+                        @kib_b.assign_attributes({kode_lokasi: kode_lokasi, nama_barang: nama_barang, nomor_register: nomor_register,
+                            tipe_barang: tipe_barang, bahan_barang: bahan_barang, tahun_pembelian: tahun_pembelian, 
+                            nomor_pabrik: nomor_pabrik, nomor_rangka: nomor_rangka, nomor_mesin: nomor_mesin, nomor_polisi: nomor_polisi, 
+                            asal_usul: asal_usul, harga_barang: harga_barang, keterangan: keterangan})
+                        @kib_b.save(:validate => false)
+                        render json: {succes: @kib_b}, status: :ok                
+                    rescue Exception => e
+                        render json: {error: "edit gagal #{e}"}, status: :unprocessable_entity
                     end
-                    if params[:nama_barang].blank?
-                        nama_barang = @kib_b.nama_barang
-                    end
-                    if params[:nomor_register].blank?
-                        nomor_register = @kib_b.nomor_register
-                    end
-                    if params[:tipe_barang].blank?
-                        tipe_barang = @kib_b.tipe_barang
-                    end
-                    if params[:bahan_barang].blank?
-                        bahan_barang = @kib_b.bahan_barang
-                    end
-                    if params[:tahun_pembelian].blank? 
-                        tahun_pembelian = @kib_b.tahun_pembelian
-                    end
-                    if params[:nomor_pabrik].blank?
-                        nomor_pabrik = @kib_b.nomor_pabrik
-                    end
-                    if params[:nomor_rangka].blank?
-                        nomor_rangka = @kib_b.nomor_rangka
-                    end
-                    if params[:nomor_mesin].blank?
-                        nomor_mesin = @kib_b.nomor_mesin
-                    end
-                    if params[:nomor_polisi].blank?
-                        nomor_polisi = @kib_b.nomor_polisi
-                    end
-                    if params[:asal_usul].blank?
-                        asal_usul = @kib_b.asal_usul
-                    end
-                    if params[:harga_barang].blank?
-                        harga_barang = @kib_b.harga_barang
-                    end
-                    if params[:keterangan].blank?
-                        keterangan = @kib_b.keterangan
-                    end
-                    @kib_b.assign_attributes({kode_lokasi: kode_lokasi, nama_barang: nama_barang, nomor_register: nomor_register,
-                        tipe_barang: tipe_barang, bahan_barang: bahan_barang, tahun_pembelian: tahun_pembelian, 
-                        nomor_pabrik: nomor_pabrik, nomor_rangka: nomor_rangka, nomor_mesin: nomor_mesin, nomor_polisi: nomor_polisi, 
-                        asal_usul: asal_usul, harga_barang: harga_barang, keterangan: keterangan})
-                    @kib_b.save(:validate => false)
-                    render json: {succes: @kib_b}, status: :ok                
-                rescue Exception => e
-                    render json: {error: "edit gagal #{e}"}, status: :unprocessable_entity
                 end
             end
         end
     end
     
     def delete
-        if params[:id].blank?
-            render json: {error: "Id harus di isi!"}, status: :unprocessable_entity
+        if role.match(/Pengguna/).present?
+            render json: {role: role, error:"Tidak memiliki akses!"}, status: :unauthorized
         else
-            @kib_b = Barang::Kibb.find(params[:id])
-            if not @kib_b.present?
-                render json: {error: "Id tidak dapat ditemukan!"}, status: :unprocessable_entity
+            if params[:id].blank?
+                render json: {error: "Id harus di isi!"}, status: :unprocessable_entity
             else
-                @kib_b.assign_attributes({status_kib: Enums::KibStatus::DELETED})
-                @kib_b.save(:validate => false)
-                render json: {success: @kib_b}, status: :ok
+                @kib_b = Barang::Kibb.find(params[:id])
+                if not @kib_b.present?
+                    render json: {error: "Id tidak dapat ditemukan!"}, status: :unprocessable_entity
+                else
+                    @kib_b.assign_attributes({status_kib: Enums::KibStatus::DELETED})
+                    @kib_b.save(:validate => false)
+                    render json: {success: @kib_b}, status: :ok
+                end
             end
         end
     end
--- backend/app/controllers/v1/kib/kibc_controller.rb
+++ backend/app/controllers/v1/kib/kibc_controller.rb
@@ -26,107 +26,119 @@ class V1::Kib::KibcController < ApplicationController
     end
   
     def create
-        @kib_c = Barang::Kibc.new(user_params)
-        if @kib_c.save
-            render json: @kib_c, status: :created
+        if role.match(/Pengguna/).present?
+            render json: {role: role, error:"Tidak memiliki akses!"}, status: :unauthorized
         else
-            render json: { errors: @kib_c.errors.full_messages }, status: :unprocessable_entity
+            @kib_c = Barang::Kibc.new(user_params)
+            if @kib_c.save
+                render json: @kib_c, status: :created
+            else
+                render json: { errors: @kib_c.errors.full_messages }, status: :unprocessable_entity
+            end
         end
     end
   
     def edit
-        if params[:id].blank?
-            render json: {error: "Id tidak boleh kosong!"}, status: :unprocessable_entity
-        else 
-            kib_validate = Barang::Kibc.where(_id: params[:id]).first
-            if not kib_validate.present?
-                render json: {error: "Id tidak dapat ditemukan!"}, status: :unprocessable_entity
-            elsif kib_validate.status_kib == Enums::KibStatus::DELETED
-                render json: {error: "Barang sudah dihapus, tidak dapat di Edit!"}, status: :unprocessable_entity
-            else
-                begin
-                    @kib_c = Barang::Kibc.find(params[:id])
-                    kode_lokasi = params[:kode_lokasi]
-                    nama_barang = params[:nama_barang]
-                    nomor_register = params[:nomor_register]
-                    kondisi_bangunan = params[:kondisi_bangunan]
-                    tingkat_bangunan = params[:tingkat_bangunan]
-                    beton_bangunan = params[:beton_bangunan]
-                    luas_lantai = params[:luas_lantai]
-                    alamat = params[:alamat]
-                    dokumen_gedung = params[:dokumen_gedung]
-                    luas = params[:luas]
-                    status = params[:status]
-                    nomor_tanah = params[:nomor_tanah]
-                    asal_usul = params[:asal_usul]
-                    harga = params[:harga]
-                    keterangan = params[:keterangan]
-                    if params[:kode_lokasi].blank? 
-                        kode_lokasi = @kib_c.kode_lokasi
+        if role.match(/Pengguna/).present?
+            render json: {role: role, error:"Tidak memiliki akses!"}, status: :unauthorized
+        else
+            if params[:id].blank?
+                render json: {error: "Id tidak boleh kosong!"}, status: :unprocessable_entity
+            else 
+                kib_validate = Barang::Kibc.where(_id: params[:id]).first
+                if not kib_validate.present?
+                    render json: {error: "Id tidak dapat ditemukan!"}, status: :unprocessable_entity
+                elsif kib_validate.status_kib == Enums::KibStatus::DELETED
+                    render json: {error: "Barang sudah dihapus, tidak dapat di Edit!"}, status: :unprocessable_entity
+                else
+                    begin
+                        @kib_c = Barang::Kibc.find(params[:id])
+                        kode_lokasi = params[:kode_lokasi]
+                        nama_barang = params[:nama_barang]
+                        nomor_register = params[:nomor_register]
+                        kondisi_bangunan = params[:kondisi_bangunan]
+                        tingkat_bangunan = params[:tingkat_bangunan]
+                        beton_bangunan = params[:beton_bangunan]
+                        luas_lantai = params[:luas_lantai]
+                        alamat = params[:alamat]
+                        dokumen_gedung = params[:dokumen_gedung]
+                        luas = params[:luas]
+                        status = params[:status]
+                        nomor_tanah = params[:nomor_tanah]
+                        asal_usul = params[:asal_usul]
+                        harga = params[:harga]
+                        keterangan = params[:keterangan]
+                        if params[:kode_lokasi].blank? 
+                            kode_lokasi = @kib_c.kode_lokasi
+                        end
+                        if params[:nama_barang].blank?
+                            nama_barang = @kib_c.nama_barang
+                        end
+                        if params[:nomor_register].blank?
+                            nomor_register = @kib_c.nomor_register
+                        end
+                        if params[:kondisi_bangunan].blank?
+                            kondisi_bangunan = @kib_c.kondisi_bangunan
+                        end
+                        if params[:tingkat_bangunan].blank? 
+                            tingkat_bangunan = @kib_c.tingkat_bangunan
+                        end
+                        if params[:beton_bangunan].blank?
+                            beton_bangunan = @kib_c.beton_bangunan
+                        end
+                        if params[:luas_lantai].blank?
+                            luas_lantai = @kib_c.luas_lantai
+                        end
+                        if params[:alamat].blank?
+                            alamat = @kib_c.alamat
+                        end
+                        if params[:dokumen_gedung].blank?
+                            dokumen_gedung = @kib_c.dokumen_gedung
+                        end
+                        if params[:luas].blank?
+                            luas = @kib_c.luas
+                        end
+                        if params[:status].blank?
+                            status = @kib_c.status
+                        end
+                        if params[:asal_usul].blank?
+                            asal_usul = @kib_c.asal_usul
+                        end
+                        if params[:harga].blank?
+                            harga = @kib_c.harga
+                        end
+                        if params[:keterangan].blank?
+                            keterangan = @kib_c.keterangan
+                        end
+                        @kib_c.assign_attributes({kode_lokasi: kode_lokasi, nama_barang: nama_barang, nomor_register: nomor_register,
+                            kondisi_bangunan: kondisi_bangunan, tingkat_bangunan: tingkat_bangunan, beton_bangunan: beton_bangunan, 
+                            luas_lantai: luas_lantai, alamat: alamat, dokumen_gedung: dokumen_gedung, luas: luas, status: status,
+                            asal_usul: asal_usul, harga: harga, keterangan: keterangan})
+                        @kib_c.save(:validate => false)
+                        render json: {succes: @kib_c}, status: :ok                
+                    rescue Exception => e
+                        render json: {error: "edit gagal #{e}"}, status: :unprocessable_entity
                     end
-                    if params[:nama_barang].blank?
-                        nama_barang = @kib_c.nama_barang
-                    end
-                    if params[:nomor_register].blank?
-                        nomor_register = @kib_c.nomor_register
-                    end
-                    if params[:kondisi_bangunan].blank?
-                        kondisi_bangunan = @kib_c.kondisi_bangunan
-                    end
-                    if params[:tingkat_bangunan].blank? 
-                        tingkat_bangunan = @kib_c.tingkat_bangunan
-                    end
-                    if params[:beton_bangunan].blank?
-                        beton_bangunan = @kib_c.beton_bangunan
-                    end
-                    if params[:luas_lantai].blank?
-                        luas_lantai = @kib_c.luas_lantai
-                    end
-                    if params[:alamat].blank?
-                        alamat = @kib_c.alamat
-                    end
-                    if params[:dokumen_gedung].blank?
-                        dokumen_gedung = @kib_c.dokumen_gedung
-                    end
-                    if params[:luas].blank?
-                        luas = @kib_c.luas
-                    end
-                    if params[:status].blank?
-                        status = @kib_c.status
-                    end
-                    if params[:asal_usul].blank?
-                        asal_usul = @kib_c.asal_usul
-                    end
-                    if params[:harga].blank?
-                        harga = @kib_c.harga
-                    end
-                    if params[:keterangan].blank?
-                        keterangan = @kib_c.keterangan
-                    end
-                    @kib_c.assign_attributes({kode_lokasi: kode_lokasi, nama_barang: nama_barang, nomor_register: nomor_register,
-                        kondisi_bangunan: kondisi_bangunan, tingkat_bangunan: tingkat_bangunan, beton_bangunan: beton_bangunan, 
-                        luas_lantai: luas_lantai, alamat: alamat, dokumen_gedung: dokumen_gedung, luas: luas, status: status,
-                        asal_usul: asal_usul, harga: harga, keterangan: keterangan})
-                    @kib_c.save(:validate => false)
-                    render json: {succes: @kib_c}, status: :ok                
-                rescue Exception => e
-                    render json: {error: "edit gagal #{e}"}, status: :unprocessable_entity
                 end
             end
         end
     end
     
     def delete
-        if params[:id].blank?
-            render json: {error: "Id harus di isi!"}, status: :unprocessable_entity
+        if role.match(/Pengguna/).present?
+            render json: {role: role, error:"Tidak memiliki akses!"}, status: :unauthorized
         else
-            @kib_c = Barang::Kibc.find(params[:id])
-            if not @kib_c.present?
-                render json: {error: "Id tidak ada!"}, status: :unprocessable_entity
+            if params[:id].blank?
+                render json: {error: "Id harus di isi!"}, status: :unprocessable_entity
             else
-                @kib_c.assign_attributes({status_kib: Enums::KibStatus::DELETED})
-                @kib_c.save(:validate => false)
-                render json: {success: @kib_c}, status: :ok
+                @kib_c = Barang::Kibc.find(params[:id])
+                if not @kib_c.present?
+                    render json: {error: "Id tidak ada!"}, status: :unprocessable_entity
+                else
+                    @kib_c.assign_attributes({status_kib: Enums::KibStatus::DELETED})
+                    @kib_c.save(:validate => false)
+                    render json: {success: @kib_c}, status: :ok
+                end
             end
         end
     end
