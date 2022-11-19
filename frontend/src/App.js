a296972d80c20681473c2ce3f8213f310c77586e
imsyad <irsyadmuhammad.020201@gmail.com>
2022-11-19 17:45:23 +0700

    :sparkles: Frontend: Setup router, and theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout.jsx";
+import Dashboard from "./pages/dashboard.jsx";
 import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Dashboard />}></Route>
+            <Route path="/pendataan" element={<Dashboard />}></Route>
+            <Route path="/pencatatan" element={<Dashboard />}></Route>
+            <Route path="/pelaporan" element={<Dashboard />}></Route>
+          </Route>
+        </Routes>
+    </React.Fragment>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    button: {
+      textTransform: "none"
+    }
+  },
+  palette: {
+    themePrimary: {
+      main: '#009B4C',
+      darkest: '#007037'
+    }
+  },
+  
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
