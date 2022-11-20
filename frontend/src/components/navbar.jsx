9dba08ef3c9d2fa10d8783ea68645a5cf5644274
imsyad <irsyadmuhammad.020201@gmail.com>
2022-11-20 18:37:34 +0700

    :pencil2: Frontend: Change writing format (PNG to png)

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -8,7 +8,7 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import { ChevronDown } from "react-feather";
 
-import NavbarLogo from '../assets/images/navbar_logo.PNG';
+import NavbarLogo from '../assets/images/navbar_logo.png';
 
 const Navbar = () => {
 
