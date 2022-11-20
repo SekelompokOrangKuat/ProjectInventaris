f1e80fdb2d5d5439dcdf7f96c01fdf632b00d105
imsyad <irsyadmuhammad.020201@gmail.com>
2022-11-20 11:45:43 +0700

    :art: Frontend: Improve encapsulation

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -11,13 +11,25 @@
  */
 
 import React from 'react';
-import { Box, Container } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Box, IconButton, Container, Typography } from "@mui/material";
+import { Box as BoxFeather, FilePlus, FileText, Home, Menu } from "react-feather";
+import { useLocation, Outlet } from "react-router-dom";
 
 import Navbar from './navbar';
 import Sidebar from './sidebar';
 
+
 const Layout = () => {
+    const menuLists = [
+        { name: "Dashboard", url: "/", icon: <Home size={20}/> },
+        { name: "Pendataan", url: "/pendataan", icon: <FilePlus size={20}/> },
+        { name: "Pencatatan", url: "/pencatatan", icon: <BoxFeather size={20}/> },
+        { name: "Pelaporan", url: "/pelaporan", icon: <FileText size={20}/> }
+    ];
+
+    const location = useLocation();
+    const currentLocationData = menuLists.reduce((result, item)=> item.url === location.pathname ? [...result, item] : result, []);
+
     return (
         <Container
             disableGutters
@@ -34,12 +46,29 @@ const Layout = () => {
                 sx={{
                     display: 'flex',
                     flexDirection: 'row',
-                    height: '100%'
+                    height:'calc(100vh - 71px)',
                 }}
             >
-                <Sidebar />
-                <Box sx={{ width: '100%', overflowY: 'auto' }}>
-                    <Outlet />
+                <Sidebar menu={menuLists}/>
+                <Box sx={{ width: '100%', overflowY: 'auto'}}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            gap: 1,
+                            px: 5,
+                            py:3,
+                            color: 'themeGrey.darkest',
+                        }}
+                    >
+                        <IconButton aria-label="Menu" sx={{color:'themePrimary.main'}}><Menu size={20} /></IconButton>
+                        {currentLocationData[0].icon}
+                        <Typography variant="h2">{currentLocationData[0].name}</Typography>
+                    </Box>
+                    <Box sx={{ width: '100%', overflowY: 'auto' }}>
+                        <Outlet />
+                    </Box>
                 </Box>
             </Container>
         </Container>
