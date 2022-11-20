9993e2956eba2f9ba49d614e3bbd6bd7fd55cc6b
imsyad <irsyadmuhammad.020201@gmail.com>
2022-11-20 11:43:57 +0700

    :art: Frontend: Separate theme file with index.js file

---
+++ frontend/src/assets/js/theme.js
@@ -0,0 +1,83 @@
+import { createTheme } from "@mui/material/styles";
+
+var theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: '34px',
+      fontWeight: 'bold'
+    },
+    h2: {
+      fontSize: '24px',
+      fontWeight: 'bold'
+    },
+    h3: {
+      fontSize: '20px',
+      fontWeight: '600'
+    },
+    h4: {
+      fontSize: '16px',
+      fontWeight: '600'
+    },
+    h5: {
+      fontSize: '14px',
+      fontWeight: '600'
+    },
+    h6: {
+      fontSize: '12px',
+      fontWeight: '600'
+    },
+    body1: {
+      fontSize: '14px'
+    },
+    caption: {
+      fontSize: '12px',
+      fontWeight: '300'
+    },
+    button: {
+      fontSize: '14px',
+      fontWeight: '600',
+      textTransform: "none",
+    }
+  },
+  palette: {
+    themePrimary: {
+      main: '#009B4C',
+      darkest: '#007037',
+    },
+    themeSecondary:{
+      main: '#1559E6',
+      darkest: '#0F2C64',
+      darker: '#0236A4',
+      lighter: '#2E6BEB',
+      lightest: '#6B9AF3',
+    },
+    themeGrey: {
+      darkest: '#212121',
+      darker: '#616161',
+      main: '#757575',
+      ligther: '#9E9E9E',
+      lightest: '#E0E0E0'
+    },
+    themeWhite: {
+      lightest:'#FFFFFF',
+    }, 
+  },
+});
+
+theme = createTheme(theme,{
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        containedPrimary: {
+          backgroundColor: 'theme.themeSecondary.main',
+        },
+        outlinedPrimary:{
+          borderColor:'theme.themeSecondary.main',
+          color:'theme.themeSecondary.darker'
+        }
+      },
+    },
+  },
+})
+
+export default theme;
