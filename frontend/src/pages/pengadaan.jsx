b4f3745667019ddc35670a26ca7c0f91ff6817e3
gopelkujo <sigantengnoval@gmail.com>
2022-11-28 15:21:18 +0700

    :alien: Frontend: Add get data table from API at Pengadaan page

--- frontend/src/pages/pengadaan.jsx
+++ frontend/src/pages/pengadaan.jsx
@@ -1,52 +1,141 @@
-import { Box, Grid, Typography, Stack, TextField, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, tableCellClasses, styled, Paper, Checkbox } from "@mui/material";
-import { Edit, PlusSquare, Trash2 } from "react-feather";
+import { Box, Grid, Typography, Stack, TextField, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, tableCellClasses, styled, Paper, IconButton, LinearProgress, Select, MenuItem } from "@mui/material";
+import { Edit, PlusSquare, Save, Trash2 } from "react-feather";
 import React from "react";
 
 const Pengadaan = () => {
 
-    /* REST API EXAMPLE */
-    // const [posts, setPosts] = React.useState([]);
-    // React.useEffect(() => {
-    //     fetch(
-    //         'https://cors-anywhere.herokuapp.com/http://backend-sinbada.herokuapp.com/v1/kib/kiba/findAll',
-    //         {
-    //             method: 'GET',
-    //             headers: {
-    //                 // 'Accept': 'application/json',
-    //                 'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjp7IiRvaWQiOiI2MzcxY2ViNmUyMWZhYzBjNDhlNGUxNmQifSwiZXhwIjoxNjY5MDMyMDE4fQ.PujpxQ882HAwkZjGAwHiUpj_LHf_FcF4OcwDNbBEuAY',
-    //                 'X-Requested-With': 'application/json'
-    //             },
-    //         }
-    //     )
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //             setPosts(data);
-    //             console.log(posts[0]['_id']['$oid'])
-    //         })
-    //         .catch((err) => {
-    //             console.log(err.message);
-    //         });
-    // }, []);
+    const [dataTable, setDataTable] = React.useState([]);
+    const [isTableUsulan, setIsTableUsulan] = React.useState(true);
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    /* FORM DATA CHANGE */
+    const [namaRuangan, setNamaRuangan] = React.useState('');
+    const [spesifikasiBarang, setSpesifikasiBarang] = React.useState('');
+    const [namaPenanggungJawab, setNamaPenanggungJawab] = React.useState('');
+    const [merkBarang, setMerkBarang] = React.useState('');
+    const [kodeBarang, setKodeBarang] = React.useState('');
+    const [keterangan, setKeterangan] = React.useState('');
+    const [namaBarang, setNamaBarang] = React.useState('');
+    const [fotoBarang, setFotoBarang] = React.useState('');
+    const [jumlahBarang, setJumlahBarang] = React.useState('');
+    const [ukuranBarang, setUkuranBarang] = React.useState('');
+    const [bahanBarang, setBahanBarang] = React.useState('');
+    const [tahunPembelian, setTahunPembelian] = React.useState('');
+    const [nomorPabrik, setNomorPabrik] = React.useState('');
+    const [nomorRangka, setNomorRangka] = React.useState('');
+    const [nomorMesin, setNomorMesin] = React.useState('');
+    const [nomorPolisi, setNomorPolisi] = React.useState('');
+    const [nomorBPKB, setNomorBPKB] = React.useState('');
+    const [asalUsul, setAsalUsul] = React.useState('');
+    const [hargaBarang, setHargaBarang] = React.useState('');
+
+    const concatData = (data1, data2, status) => {
+        let concatedData = [];
+        for (let i = 0; i < data2.length; i++) {
+            const joinedObject = { ...data1[i], ...data2[i] };
+            if (status === 0) {
+                if (joinedObject.status_usulan === 0) {
+                    concatedData.push(joinedObject);
+                }
+            } else {
+                if (joinedObject.status_usulan !== 0) {
+                    concatedData.push(joinedObject);
+                }
+            }
+        }
+        setDataTable(concatedData);
+    }
+
+    const addBarang = () => {
+
+    }
+
+    const getDataTable = async (status) => {
+        setIsLoading(true);
+        await fetch(
+            'http://backend.icygrass-3ea20227.eastasia.azurecontainerapps.io/v1/user/pengadaan/findAll',
+            {
+                method: "GET",
+                headers: {
+                    'Accept': 'application/json',
+                    'X-Requested-With': 'application/json',
+                    'Content-type': 'application/json; charset=UTF-8',
+                    'Access-Control-Allow-Origin': '*',
+                    'Authorization': localStorage.getItem('token'),
+                }
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.response_code === 200) {
+                    console.log(data.data);
+                    concatData(
+                        data.data.pengadaan,
+                        data.data.barang,
+                        status);
+                } else {
+                    console.log('error' + data.response_code + data.response_message);
+                }
+            })
+            .catch((err) => {
+                console.log('error: ' + err.message);
+            });
+        setIsLoading(false);
+    }
+
+    const getStatusCode = (status) => {
+        switch (status) {
+            case 1:
+                return "Accepted";
+                break;
+            case 2:
+                return "Rejected";
+                break;
+            case 3:
+                return "New";
+                break;
+            default:
+                return "Deleted";
+                break;
+        }
+    }
+
+    React.useEffect(() => {
+        getDataTable(0);
+    }, []);
 
     const fields = [];
 
     const labels = [
-        "Nama Ruangan",
-        "Spesifikasi",
-        "Nama Penanggung Jawab",
-        "Merk/Type",
-        "Kode Barang",
-        "Keterangan",
-        "Nama Barang",
-        "Upload Gambar",
-        "Jumlah Barang",
+        { name: "Nama Ruangan", controller: setNamaRuangan, value: namaRuangan },
+        { name: "Spesifikasi", controller: setSpesifikasiBarang, value: spesifikasiBarang },
+        { name: "Nama Penanggung Jawab", controller: setNamaPenanggungJawab, value: namaPenanggungJawab },
+        { name: "Merk/Type", controller: setMerkBarang, value: merkBarang },
+        { name: "Kode Barang", controller: setKodeBarang, value: kodeBarang },
+        { name: "Keterangan", controller: setKeterangan, value: keterangan },
+        { name: "Nama Barang", controller: setNamaBarang, value: namaBarang },
+        { name: "Upload Gambar", controller: setFotoBarang, value: fotoBarang },
+        { name: "Jumlah Barang", controller: setJumlahBarang, value: jumlahBarang },
+        { name: "Ukuran Barang", controller: setUkuranBarang, value: ukuranBarang },
+        { name: "Bahan Barang", controller: setBahanBarang, value: bahanBarang },
+        { name: "Tahun Pembelian", controller: setTahunPembelian, value: tahunPembelian },
+        { name: "Nomor Pabrik", controller: setNomorPabrik, value: nomorPabrik },
+        { name: "Nomor Rangka", controller: setNomorRangka, value: nomorRangka },
+        { name: "Nomor Mesin", controller: setNomorMesin, value: nomorMesin },
+        { name: "Nomor Polisi", controller: setNomorPolisi, value: nomorPolisi },
+        { name: "Nomor BPKB", controller: setNomorBPKB, value: nomorBPKB },
+        { name: "Asal Usul", controller: setAsalUsul, value: asalUsul },
+        { name: "Harga Barang", controller: setHargaBarang, value: hargaBarang },
     ];
+
+    const handleChangeInput = (controller, value) => {
+        controller(value.target.value);
+    };
 
     for (let i = 0; i < labels.length; i++) {
         fields.push(
             <Grid item xs={4}>
-                <TextField variant="outlined" label={labels[i]} fullWidth />
+                <TextField variant="outlined" label={labels[i].name} onChange={(e) => handleChangeInput(labels[i].controller, e)} fullWidth />
             </Grid>
         );
     }
@@ -71,26 +160,6 @@ const Pengadaan = () => {
             backgroundColor: "#D9E6FF",
         },
     }));
-
-    function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
-    }
-
-    const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-    ];
 
     return (
         <Box
@@ -188,6 +257,53 @@ const Pengadaan = () => {
                 </Box>
             </Stack>
 
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                maxWidth="100%"
+                sx={{
+                    px: 3,
+                    mb: 2,
+                    boxSizing: "border-box"
+                }}
+            >
+                <Grid item>
+                    <Select
+                        value={isTableUsulan}
+                        onChange={() => {
+                            getDataTable(!isTableUsulan ? 0 : 1)
+                            setIsTableUsulan(!isTableUsulan);;
+                        }}
+                    >
+                        <MenuItem value={true}>Usulan</MenuItem>
+                        <MenuItem value={false}>Riwayat</MenuItem>
+                    </Select>
+                </Grid>
+                <Grid item>
+                    <TextField
+                        variant="outlined"
+                        label="Search"
+                    // onChange={
+                    //     (e) => {
+                    //         if (e.target.value !== '') {
+                    //             getSearchDataTable(e.target.value);
+                    //         } else {
+                    //             getDataTable(isTableUsulan ? 0 : 1);
+                    //         }
+                    //     }
+                    // }
+                    />
+                </Grid>
+            </Grid>
+
+            {
+                isLoading
+                    ? <LinearProgress m={3} />
+                    : null
+            }
+
             <TableContainer
                 component={Paper}
                 sx={{
@@ -214,30 +330,69 @@ const Pengadaan = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map((row, index) => (
+                        {dataTable.map((item, index) => (
                             <StyledTableRow key={index}>
+                                {
+                                    isTableUsulan
+                                        ? <StyledTableCell align="center" sx={{ border: 1 }}>
+                                            <Stack
+                                                direction="row"
+                                                justifyContent="center"
+                                                alignItems="center"
+                                                spacing={2}
+                                            >
+                                                <IconButton >
+                                                    <Edit size={20} />
+                                                </IconButton>
+                                                <IconButton>
+                                                    <Trash2 size={20} />
+                                                </IconButton>
+                                            </Stack>
+                                        </StyledTableCell>
+                                        : null
+                                }
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{index + 1}</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{item.nama_ruangan}</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{item.nama_pengusul}</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{item.kode_barang}</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{item.nama_barang}</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{item.jumlah_barang}</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{item.spesifikasi_barang}</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{item.tipe_barang}</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{item.keterangan}</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ border: 1 }}>{item.foto_barang}</StyledTableCell>
                                 <StyledTableCell align="center" sx={{ border: 1 }}>
-                                    <Stack
-                                        direction="row"
-                                        justifyContent="center"
-                                        alignItems="center"
-                                        spacing={2}
-                                    >
-                                        <Checkbox sx={{ margin: "0", padding: "0" }} size="medium" />
-                                        <Edit size={20} />
-                                        <Trash2 size={20} />
-                                    </Stack>
+                                    {
+                                        item.status_usulan === 0
+                                            ?
+                                            <Stack
+                                                direction="row"
+                                                justifyContent="center"
+                                                alignItems="center"
+                                                spacing={1}
+                                            >
+                                                <Button
+                                                    variant="outlined"
+                                                    startIcon={<Save />}
+                                                    color="success"
+                                                    sx={{ minWidth: "108px" }}
+                                                // onClick={() => approvalBarang(item.user_pengusulan_id.$oid, "true")}
+                                                >
+                                                    <Typography variant="button">Terima</Typography>
+                                                </Button>
+                                                <Button
+                                                    variant="outlined"
+                                                    startIcon={<Save />}
+                                                    color="error"
+                                                    sx={{ minWidth: "108px" }}
+                                                // onClick={() => approvalBarang(item.user_pengusulan_id.$oid, "false")}
+                                                >
+                                                    <Typography variant="button">Tolak</Typography>
+                                                </Button>
+                                            </Stack>
+                                            : getStatusCode(item.status_usulan)
+                                    }
                                 </StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{index}</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{row.calories}</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{row.fat}</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{row.carbs}</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{row.protein}</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{row.calories}</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{row.fat}</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{row.carbs}</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{row.protein}</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ border: 1 }}>{row.calories}</StyledTableCell>
                             </StyledTableRow>
                         ))}
                     </TableBody>
